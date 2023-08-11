8121810cde642b71e1f12b102e857601fac5d79a
Huyxu2K <nguyenvanhuy2712000@gmail.com>
2023-08-11 14:01:27 +0700

    send request with auth

--- grpc/src/client.rs
+++ grpc/src/client.rs
@@ -1,5 +1,8 @@
 use books::books_client::BooksClient;
-use books::BookRequest;
+use books::{BookInfomation, BookInsertRequest};
+use tonic::Request;
+use tonic::{metadata::MetadataValue};
+use tonic::transport::Channel;
 
 pub mod books {
     tonic::include_proto!("books");
@@ -7,16 +10,23 @@ pub mod books {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = BooksClient::connect("http://127.0.0.1:8080").await?;
-
-    let req = tonic::Request::new(BookRequest {
+    let channel = Channel::from_static("http://[::1]:50051").connect().await?;
+    let bookinfo= BookInfomation {
         name: "math".to_string(),
         author: "huyxu".to_string(),
         quantity: "5".to_string(),
         description: "test".to_string(),
+    };
+    let token: MetadataValue<_>="Bearer some-auth-token".parse()?;
+    let mut client=BooksClient::with_interceptor(channel,move|mut req:Request<()>|{
+        req.metadata_mut().insert("authorization", token.clone());
+        Ok(req)
     });
-   let res=client.insert(req).await?;
+    let req = tonic::Request::new(BookInsertRequest {
+        bookinfo:Some(bookinfo),
+    });
+    let res = client.insert(req).await?;
 
-   println!("Response={:?}",res);
-   Ok(())
+    println!("Response={:?}", res);
+    Ok(())
 }
